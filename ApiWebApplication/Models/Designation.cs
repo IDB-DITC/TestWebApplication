9c853f704538ad95bd36295641e44f1ed85c1e81
idbditc@gmail.com <idbditc@gmail.com>
2024-01-10 18:48:47 +0600

    Add project files.

---
+++ ApiWebApplication/Models/Designation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiWebApplication.Models
+{
+	[Table("tblDesignation")]
+	public class Designation
+	{
+		[Key]
+        public int DesignationId { get; set; }
+
+		[Required]
+        public string DesignationName { get; set; }
+		public virtual IList<Employee>? Employees { get; set; }
+
+	}
+}
---
+++ ApiWebApplication/Models/EmpDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiWebApplication.Models
+{
+	public class EmpDb : DbContext
+	{
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Experience> Experiences { get; set; }
+		public DbSet<Designation> Designations { get; set; }
+
+
+		public EmpDb(DbContextOptions<EmpDb> o) : base(o)
+		{
+
+		}
+	}
+}
